51d94becd5b259157e0b414046f89c118d7c37e1
madeyedexter <madeyedexter92@gmail.com>
2016-08-05 11:10:59 +0530

    Added Settings Activity and ToolBar Added XML Resources for Material Design Icon Sets

--- app/src/main/java/com/example/madey/easynotes/MainActivity.java
+++ app/src/main/java/com/example/madey/easynotes/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.madey.easynotes;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -16,19 +20,13 @@ public class MainActivity extends AppCompatActivity {
     private FloatingActionButton fab1;
     private FloatingActionButton fab2;
     private FloatingActionButton fab3;
+    private FloatingActionButton fab4;
 
     private View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
-                case R.id.fab1:
-                    break;
-                case R.id.fab2:
-                    fab2.setVisibility(View.GONE);
-                    break;
-                case R.id.fab3:
-                    fab2.setVisibility(View.VISIBLE);
-                    break;
+
             }
         }
     };
@@ -39,17 +37,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        myToolbar.setTitle("EasyNotes");
+
         menuRed = (FloatingActionMenu) findViewById(R.id.menu_red);
 
-        fab2 = (FloatingActionButton) findViewById(R.id.fab2);
-        fab3 = (FloatingActionButton) findViewById(R.id.fab3);
-        fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        fab1 = (FloatingActionButton) findViewById(R.id.fab_list);
+        fab2 = (FloatingActionButton) findViewById(R.id.fab_note);
+        fab3 = (FloatingActionButton) findViewById(R.id.fab_audio_note);
+        fab4 = (FloatingActionButton) findViewById(R.id.fab_video_note);
 
         menuRed.setClosedOnTouchOutside(true);
 
         menuRed.setVisibility(View.VISIBLE);
 
-        menuRed.hideMenuButton(false);
         fab1.setOnClickListener(clickListener);
         fab2.setOnClickListener(clickListener);
         fab3.setOnClickListener(clickListener);
@@ -57,15 +60,30 @@ public class MainActivity extends AppCompatActivity {
         menuRed.setOnMenuButtonClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (menuRed.isOpened()) {
-                    Toast.makeText(MainActivity.this, menuRed.getMenuButtonLabelText(), Toast.LENGTH_SHORT).show();
-                }
-
                 menuRed.toggle(true);
             }
         });
-
         menuRed.showMenuButton(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        menu.getItem(1).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            }
+        });
+        return true;
+    }
+
+    public void launchSettings(){
 
     }
 }
